fb9611ef0d1d2615d1657c44475bcfe6a732dff1
Charlene Nicol <charln.nicol@gmail.com>
2023-01-17 18:15:37 +0100

    routes changed

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :prestations
+  root to: "prestations#index"
   resources :buyings, only: %i[index create new destroy]
 end
